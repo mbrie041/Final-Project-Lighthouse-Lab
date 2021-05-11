cbdbcd3dd4d49aab5c90b146690ac18549e78c74
Jacqueline <jlee4332@gmail.com>
2021-05-11 11:20:37 -0400

    delete outside files & add score/time to levelone-scene

--- frontend/src/phaser/levelone-scene.js
+++ frontend/src/phaser/levelone-scene.js
@@ -1,6 +1,11 @@
 import Phaser from "phaser";
 import Player from "./player.js";
 
+let scoreText;
+let timeText;
+let pancake;
+
+
 export default class LevelOneScene extends Phaser.Scene {
   constructor() {
     super("LevelOneScene");
@@ -47,10 +52,9 @@ export default class LevelOneScene extends Phaser.Scene {
     "pancake",
     "src/assets/images/Pancake_Stack (16 x 16).png"
     );
-    }
+  }
     
-    create() {
-    
+  create() {
     const map = this.make.tilemap({ key: "level1map" });
     const groundTiles = map.addTilesetImage(
     "Gray_Tile_Terrain (16 x 16)",
@@ -76,8 +80,7 @@ export default class LevelOneScene extends Phaser.Scene {
     map.createLayer("ExitDoor", exitDoorTiles)
     map.createLayer("ExitSign", exitSignTiles)
     
-    
-    
+  
     this.scaffoldingLayer = map.createLayer("Scaffolding", scaffoldingTiles)
     this.groundLayer = map.createLayer("Ground", groundTiles);
   
@@ -99,6 +102,40 @@ export default class LevelOneScene extends Phaser.Scene {
     this.cameras.main.startFollow(this.player.sprite);
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
+    scoreText = this.add
+      .text(20, 0, `Score: ${global.score}`, { 
+        fontSize: '16px', 
+        fill: '#ffffff' 
+    }) 
+    .setScrollFactor(0);
+
+     //timer text
+    timeText = this.add
+     .text(250, 0,`Time: ${global.elaspedTime}`, {
+       fontSize: '16px', 
+       fill: '#ffffff' 
+     })
+     .setScrollFactor(0);
+
+          //populate pancake group and populates it. Repeats x amount of times and spreads them stepX apart
+    pancake = this.physics.add.group({
+      key:'pancake',
+      repeat: 20,
+      setXY: {x: 400, y:0, stepX: 100}
+    });
+
+   //set bounce when items are initially dropped 
+    pancake.children.iterate(function (child) {
+
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.6));
+
+       //pancakes will collide with ground layer to keep them from falling off page
+       
+      });
+      this.physics.add.collider(pancake, this.groundLayer && this.scaffoldingLayer);
+  
+      this.physics.add.overlap(this.player.sprite, pancake, collectItem, null);
+
     // Help text that has a "fixed" position on the screen
     // this.add
     //   .text(16, 16, "Arrow keys or WASD to move & jump", {
@@ -109,6 +146,7 @@ export default class LevelOneScene extends Phaser.Scene {
     //   })
     //   .setScrollFactor(0);
   }
+
   update(time, delta) {
     // Allow the player to respond to key presses and move itself
     this.player.update();
@@ -117,5 +155,29 @@ export default class LevelOneScene extends Phaser.Scene {
       this.player.destroy();
       this.scene.restart();
     }
+    displayTimeElapsed(time)
   }
 };
+
+
+function collectItem (player, item) {
+  console.log("COLLISION WITH ITEM!")
+  item.disableBody(`${item}`,`${item}`)
+  global.score += 10;
+  scoreText.setText('Score: ' + global.score);
+}
+
+
+function displayTimeElapsed(time) {
+  global.elapsedTime = time * .001;
+  let min = Math.floor(global.elapsedTime / 60);
+  let sec = (global.elapsedTime % 60).toFixed(2);
+
+  if (min < 10) {
+      min = '0' + min;
+  }
+  if (sec < 10) {
+      sec = '0' + sec;
+  }
+  timeText.setText('Time: ' + min + ':' + sec);
+} 
