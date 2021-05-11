d61b34229151f330a003c67aed85a0cfcca166c4
Ruowen Tang <ruowen.tang@gmail.com>
2021-05-11 00:18:47 -0400

    Display score data

--- frontend/src/components/LeaderboardScore.jsx
+++ frontend/src/components/LeaderboardScore.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function LeaderboardScore() {
-
+export default function LeaderboardScore(props) {
+  console.log('props (leaderboardscore.jsx) = ', props)
+  return (
+    <ul>
+      <li>ID: {props.id}</li>
+      <li>Score: {props.score}</li>
+    </ul>
+  );
 }
