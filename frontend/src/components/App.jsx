a78a241068871146dfa2b86b1c9ac087635e643a
Ruowen Tang <ruowen.tang@gmail.com>
2021-05-13 10:09:13 -0400

    Clean up unused state

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,38 +4,12 @@ import "./App.scss"
 import { Table } from 'react-bootstrap';
 
 function App() {
-	const [newScore, setNewScore] = useState({});
-
-
 
 	return (
 		<div striped bordered hover variant="dark" className="app">
-			<Leaderboard newScore={newScore} />
+			<Leaderboard />
 		</div>
 	);
 }
 
 export default App;
-
-// export default class App extends React.Component {
-
-
-// 	useEffect() {
-// 		const socket = new WebSocket('ws://localhost:3001');
-// 		socket.addEventListener('open', function (event) {
-// 			console.log("connected to server");
-
-// 		});
-
-// 		socket.addEventListener('message', function (event) {
-// 			console.log("message from server ", event.data);
-// 		})
-// 	}
-// 	render() {
-// 		return (
-// 			<div striped bordered hover variant="dark" className="app">
-// 				<Leaderboard />
-// 			</div>
-// 		);
-// 	}
-// }
