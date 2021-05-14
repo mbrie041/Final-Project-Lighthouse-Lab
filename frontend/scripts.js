1e4d681d30a8a516d6a252bc7fe5d707b5ebeaad
Jacqueline <jlee4332@gmail.com>
2021-05-14 16:12:42 -0400

    add popup feature

---
+++ frontend/scripts.js
@@ -0,0 +1,38 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const popup = document.querySelector(button.dataset.modalTarget);
+    openModal(popup);
+  })
+});
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const popup = document.getElementById('popup');
+    console.log('clicked!!!!!', popup)
+    closeModal(popup);
+  })
+});
+
+overlay.addEventListener('click', ()=> {
+  const popup = document.querySelectorAll('.popup.active');
+  popup.forEach(popup => {
+    closeModal(popup);
+  })
+})
+
+function openModal(popup) {
+  console.log('after!!!!!', popup)
+  if (popup == null) return
+  popup.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(popup) {
+  if (popup == null) return
+  popup.classList.remove('active');
+  overlay.classList.remove('active');
+}
