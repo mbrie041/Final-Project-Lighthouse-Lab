fed881d5a857cfcf2d3ac1cc345033e5b354a439
Ruowen Tang <ruowen.tang@gmail.com>
2021-05-15 12:45:06 -0400

    Define ws listener in chatServer

--- backend-api/bin/chatServer.js
+++ backend-api/bin/chatServer.js
@@ -16,19 +16,75 @@ const server = http.createServer(chatApp);
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server });
 
+/**
+ * 
+ */
+const chatHistory = [];
+const msgTypes = {
+  fullChatHistory: 'fullChatHistory',
+  newMessage: 'newMessage',
+  newUser: 'newUser'
+};
+
 wss.on("connection", socket => {
-  console.log("A client connected.");
-  // socket.send("Welcome New Client!");
+  handleNewConnection(socket);
 
-  // socket.onmessage = event => {
-  //   console.log(`Message Received event.data: ${event.data}`);
-  //   console.log(`Message Received event: ${event}`);
-
-  //   if (event.data === "ping") {
-  //     socket.send(JSON.stringify("pong"));
-  //   }
-  // };
+  socket.onmessage = event => {
+    console.log(`Message Received: ${JSON.stringify(event.data)}`);
+    const data = JSON.parse(event.data);
+    switch (data.type) {
+      case msgTypes.newMessage:
+        handleNewMessage(data.message);
+        break;
+      case msgTypes.newUser:
+        handleNewUser(data.user);
+        break;
+      default:
+        console.log(`Unknown message type: ${data.type}`);
+    }
+  };
 });
+
+function handleNewConnection(socket) {
+  const msg = {
+    type: msgTypes.fullChatHistory,
+    chatHistory
+  };
+
+  console.log(`A new client connected, sending chat history ${JSON.stringify(msg)}`);
+  socket.send(JSON.stringify(msg));
+}
+
+function handleNewMessage(message) {
+  chatHistory.push(message);
+  console.log(`Updated Chat History: \n${JSON.stringify(chatHistory)}`);
+
+  const newMsgData = {
+    type: msgTypes.newMessage,
+    message: message
+  };
+
+  console.log(`Broadcasting new message: ${JSON.stringify(newMsgData)}`);
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(newMsgData));
+    }
+  });
+}
+
+function handleNewUser(user) {
+  const newUserData = {
+    type: msgTypes.newUser,
+    user
+  };
+
+  console.log(`Broadcasting new user: ${JSON.stringify(newUserData)}`);
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(newUserData));
+    }
+  });
+}
 
 module.exports = {
   server,
