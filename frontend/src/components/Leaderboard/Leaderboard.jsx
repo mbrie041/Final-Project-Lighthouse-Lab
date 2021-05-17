2f38e75810f3db320e9a285ce66179c8001748dd
Ruowen Tang <ruowen.tang@gmail.com>
2021-05-17 14:30:20 -0400

    Refactor leaderboard (moved usestate to hooks

--- frontend/src/components/Leaderboard/Leaderboard.jsx
+++ frontend/src/components/Leaderboard/Leaderboard.jsx
@@ -1,39 +1,11 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import LeaderboardScoreList from "./LeaderboardScoreList.jsx";
+import useApplicationData from "../../hooks/useApplicationData";
 import { Table } from 'react-bootstrap';
 
 
 export default function Leaderboard() {
-  const [stats, setStats] = useState([]);
-
-  useEffect(() => {
-    const getStats = axios.get('http://localhost:3001/api/stats');
-
-    getStats
-      .then(response => {
-        setStats([...response.data]);
-      });
-  }, []);
-
-  useEffect(() => {
-    const socket = new WebSocket('ws://localhost:3001');
-    socket.addEventListener('open', function (event) {
-      console.log("connected to server");
-    });
-
-    socket.addEventListener('message', function (event) {
-
-      const statsObj = JSON.parse(event.data);
-      console.log("message from server ", statsObj);
-
-      if (statsObj.type === "UPDATE_LEADERBOARD") {
-        setStats(prev => [...prev, statsObj]);
-      }
-
-    });
-    return () => socket.close();
-  }, [])
+  const { state } = useApplicationData();
 
   return (
     <>
@@ -48,11 +20,10 @@ export default function Leaderboard() {
           </tr>
         </thead>
         <tbody>
-          <LeaderboardScoreList stats={stats} />
+          <LeaderboardScoreList stats={state.stats} />
         </tbody>
       </table>
     </>
-
   );
 }
 
--- frontend/src/components/Leaderboard/LeaderboardScore.jsx
+++ frontend/src/components/Leaderboard/LeaderboardScore.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 export default function LeaderboardScore(props) {
   return (
     <tr className="board-results">
