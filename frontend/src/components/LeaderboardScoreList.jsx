d012ac7e337c3bbb086274a06ed409c0e93c685f
Ruowen Tang <ruowen.tang@gmail.com>
2021-05-17 01:29:51 -0400

    Update sorted scores

--- frontend/src/components/LeaderboardScoreList.jsx
+++ frontend/src/components/LeaderboardScoreList.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import LeaderboardScore from "./LeaderboardScore.jsx";
 
 export default function LeaderboardScoreList(props) {
-
   const { stats } = props;
-  console.log('props.stats', stats);
-  return stats.map(stat => {
+
+  const sortByScores = (a, b) => {
+    if (a.score < b.score) {
+      return 1;
+    }
+    if (a.score > b.score) {
+      return -1;
+    }
+    return 0;
+  }
+
+  const sortedStats = stats.sort(sortByScores);
+
+  return sortedStats.map(stat => {
     return <LeaderboardScore key={stat.id} {...stat} />
   });
-
 };
 
