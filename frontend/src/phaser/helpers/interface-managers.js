6758f1fc343373edf8bdcffbf35cf77a0afa7a82
Jacqueline <jlee4332@gmail.com>
2021-05-19 14:12:49 -0400

    fix timer format

--- frontend/src/phaser/helpers/interface-managers.js
+++ frontend/src/phaser/helpers/interface-managers.js
@@ -27,8 +27,8 @@ function finalTimeSetter() {
   global.finalTimer = global.elapsedTime;
   //format timer
   let min = Math.floor(global.finalTimer / 60);
-  let sec = (global.finalTimer % 60).toFixed(2);
-  let mili = (((global.finalTimer % 60) % 1) * 100).toFixed(0);
+  let sec = (global.elapsedTime % 60).toFixed(0);
+  let mili = (((global.elapsedTime % 60) % 1) * 100).toFixed(0);
   if (min < 10) {
     min = "0" + min;
   }
