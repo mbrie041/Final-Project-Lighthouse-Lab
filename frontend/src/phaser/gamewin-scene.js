644834e1845e86a562111d2efc1e17a42701de6b
mbrie041 <mbrie041@gmail.com>
2021-05-18 22:16:16 -0400

    Complete Soundtrack

--- frontend/src/phaser/gamewin-scene.js
+++ frontend/src/phaser/gamewin-scene.js
@@ -4,7 +4,7 @@ import Player from "./characters/player.js";
 export default class GameWinScene extends Phaser.Scene {
   constructor() {
     super("GameWinScene");
-    this.gameWinTheme
+    this.gameWinTheme;
   }
   // init(data) {
   //   this.location = data.location;
@@ -22,7 +22,7 @@ export default class GameWinScene extends Phaser.Scene {
       .text(20, 5, `Score: ${global.score}`, {
         fontSize: "10px",
         fill: "#ffffff",
-        fontFamily: ' "Press Start 2P" '
+        fontFamily: ' "Press Start 2P" ',
       })
       .setScrollFactor(0);
     // Display Life
@@ -30,70 +30,74 @@ export default class GameWinScene extends Phaser.Scene {
       .text(150, 5, `Life: ${global.life}`, {
         fontSize: "10px",
         fill: "#ffffff",
-        fontFamily: ' "Press Start 2P" '
+        fontFamily: ' "Press Start 2P" ',
       })
       .setScrollFactor(0);
 
     // Display Timer
-  
+
     this.add
       .text(250, 5, `Time: ${global.finalTimer}`, {
         fontSize: "10px",
         fill: "#ffffff",
-        fontFamily: ' "Press Start 2P" '
+        fontFamily: ' "Press Start 2P" ',
       })
       .setScrollFactor(0);
-  
+
     this.nameInput = this.add.dom(410, 340).createFromCache("form");
-    this.message = this.add.text(130, 100, "You Win!", {
-      color: "#FFFFFF",
-      fontSize: "25px",
-      fontStyle: "bold",
-      align: "center"
-    }).setOrigin(0);
-    this.message = this.add.text(130, 150, "ENTER YOUR NAME\n\n\n\nPRESS RETURN TO CONITNUE", {
-      color: "#FFFFFF",
-      fontSize: "10px",
-      align: "center"
-    }).setOrigin(0);
-    // Display GEO location data
-    // this.locationText = this.add.text(200, 300,
-    // `[${this.location.coordinates[1]}, ${this.location.coordinates[0]}]`,
-    // {
-    // color: "#FFFFFF",
-    // fontSize: 20
-    // }
-    // ).setOrigin(0.5);
+    this.message = this.add
+      .text(130, 100, "You Win!", {
+        color: "#FFFFFF",
+        fontSize: "25px",
+        fontStyle: "bold",
+        align: "center",
+      })
+      .setOrigin(0);
+    this.message = this.add
+      .text(130, 150, "ENTER YOUR NAME\n\n\n\nPRESS RETURN TO CONITNUE", {
+        color: "#FFFFFF",
+        fontSize: "10px",
+        align: "center",
+      })
+      .setOrigin(0);
+
     console.log(`geolocation data = [${global.latitude}, ${global.longitude}]`);
 
-    this.returnKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+    this.returnKey = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.ENTER
+    );
 
-    this.returnKey.on("down", event => {
-      this.gameWinTheme.stop()
+    this.returnKey.on("down", (event) => {
+      this.gameWinTheme.stop();
       let name = this.nameInput.getChildByName("name");
       if (name.value != "" && global.aboutToChange === 1) {
         global.aboutToChange = 0;
         // post score and username to database
-        fetch('http://localhost:3001/api/stats', {
-          'method': 'POST',
+        fetch("http://localhost:3001/api/stats", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
-          'body': JSON.stringify({ 'score': global.score, 'name': name.value, 'time': global.finalTimer, 'geolocation': global.latitude })
+          body: JSON.stringify({
+            score: global.score,
+            name: name.value,
+            time: global.finalTimer,
+            geolocation: global.latitude,
+          }),
         })
-          .then(response => response.json())
-          .then(data => {
-            console.log('Success:', data);
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Success:", data);
             global.life = 3;
             global.score = 0;
             global.time = 0;
-            this.scene.start('IntroScene');
-            this.scene.stop('GameWinScene');
+            this.scene.start("IntroScene");
+            this.scene.stop("GameWinScene");
           })
           .catch((error) => {
-            console.error('Error:', error);
+            console.error("Error:", error);
           });
       }
     });
   }
-};
+}
