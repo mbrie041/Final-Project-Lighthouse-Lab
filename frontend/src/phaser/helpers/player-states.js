52d104300575dd4905e47ab8bac5418044ea346e
mbrie041 <mbrie041@gmail.com>
2021-05-20 10:18:47 -0400

    player states clean up

--- frontend/src/phaser/helpers/player-states.js
+++ frontend/src/phaser/helpers/player-states.js
@@ -1,4 +1,5 @@
 function playerDied(player) {
+  //runs the player death annimation event and then kills the player
   player.sprite.setFlipY(true);
   player.sprite.setVelocityX(0);
   player.sprite.anims.play("player-death");
@@ -6,6 +7,7 @@ function playerDied(player) {
   );
 }
 function playerFinish(player) {
+  //runs the player victory annimation event
   player.setVelocityX(0);
   player.setAccelerationX(0);
   player.setAccelerationY(0);
