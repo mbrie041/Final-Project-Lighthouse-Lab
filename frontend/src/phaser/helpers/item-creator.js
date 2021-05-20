ae1ea5ccfd9c3218e741a6f7930120b142179dec
mbrie041 <mbrie041@gmail.com>
2021-05-20 10:16:59 -0400

    item creator clean up

--- frontend/src/phaser/helpers/item-creator.js
+++ frontend/src/phaser/helpers/item-creator.js
@@ -1,25 +1,28 @@
 export default function createItem(objects, item, collectCallback, physics, layerArray, player) {
-    // const objects = map.getObjectLayer("Gems").objects;
+    //creates a placeholder array and spreads the item objects to it
     const newArray = [...objects];
+    //create new group of items and gives initial xy locations
     const newItemGroup = physics.add.group({
       key: item,
       repeat: newArray.length - 1,
       setXY: { x: 400, y: 0, stepX: 100 },
     });
 
-    //set bounce when items are initially dropped
     newItemGroup.children.iterate(function (child) {
+      //iterate through each item object and remove the item once it's been placed at it's location
       const loadedData = newArray.pop();
+      //location info from the levels json file
       child.x = loadedData.x;
       child.y = loadedData.y;
+      //sets up bounce and gravity
       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.6));
       child.body.setAllowGravity(false)
     });
-    //pancakes will collide with ground layer to keep them from falling off page
+    //item will given layers to keep them from falling off page
     for (let layer of layerArray) {
       physics.add.collider(newItemGroup, layer);
     }
 
-    //collects on player and pancake overlap
+    //collects on player and item overlap while passing in the sound effect call back
     physics.add.overlap(player, newItemGroup, collectCallback, null);
   }
